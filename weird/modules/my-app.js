722a5df4189cbf669366b47dbf3630bb31b9455f
AmanKhys <amankhys2112@gmail.com>
2023-02-16 23:24:44 +0530

    workout weirdjs working with modules

---
+++ weird/modules/my-app.js
@@ -0,0 +1,4 @@
+// default ..This is ideal for modules that export a single class or function.
+import { retunTimes as times } from './super-helpful';
+import { ludu } from './super-helpful';
+times(223,ludu);//=
---
+++ weird/modules/super-helpful.js
@@ -0,0 +1,10 @@
+export default 'allThings';
+
+export const retunTimes = (e,i) => e*i;
+
+const baa = 'sss';
+const ludu = 222;
+export{
+    baa,
+    ludu,
+}
